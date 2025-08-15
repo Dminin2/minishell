cac2b64f296fc88046a1e8242998f3d4e89ee3c5
Dminin2 <apnaeom22@gmail.com>
2025-08-15 21:27:33 +0900

    minishell.h: add readline

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,13 +6,19 @@
 /*   By: aomatsud <aomatsud@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/06 16:54:21 by aomatsud          #+#    #+#             */
-/*   Updated: 2025/08/14 15:08:14 by aomatsud         ###   ########.fr       */
+/*   Updated: 2025/08/15 18:50:12 by aomatsud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include "readline.h"
+# include "types.h"
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <stdio.h>
+# include <stdlib.h>
 # include <unistd.h>
 
 #endif
