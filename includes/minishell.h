7f907b84ff5832c740e90a5b7164411fafd153b8
Dminin2 <apnaeom22@gmail.com>
2025-08-19 16:25:39 +0900

    minishell.h: add executor

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,19 +6,26 @@
 /*   By: aomatsud <aomatsud@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/06 16:54:21 by aomatsud          #+#    #+#             */
-/*   Updated: 2025/08/15 18:50:12 by aomatsud         ###   ########.fr       */
+/*   Updated: 2025/08/18 09:05:07 by aomatsud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include "execute.h"
+# include "libft.h"
 # include "readline.h"
 # include "types.h"
+# include "utils.h"
+# include <errno.h>
+# include <error.h>
 # include <readline/history.h>
 # include <readline/readline.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <string.h>
+# include <sys/types.h>
+# include <sys/wait.h>
 # include <unistd.h>
-
 #endif
