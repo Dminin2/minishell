53bf564334740b6d6e4a7a5a216b07c2b3375f84
Dminin2 <apnaeom22@gmail.com>
2025-08-19 16:27:05 +0900

    types.h: add t_cmd

--- includes/types.h
+++ includes/types.h
@@ -6,17 +6,23 @@
 /*   By: aomatsud <aomatsud@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/15 17:21:17 by aomatsud          #+#    #+#             */
-/*   Updated: 2025/08/15 17:22:00 by aomatsud         ###   ########.fr       */
+/*   Updated: 2025/08/18 20:44:57 by aomatsud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TYPES_H
 # define TYPES_H
 
+typedef struct s_cmd
+{
+	char	*path;
+	char	**args;
+}			t_cmd;
+
 typedef enum e_status
 {
 	SUCCESS,
-	ERR_MALLOC
-}	t_status;
+	ERR_SYSTEM
+}			t_status;
 
 #endif
