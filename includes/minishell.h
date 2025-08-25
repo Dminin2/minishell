7551ed0a1d32243820f84dc70e774b7920b0b36f
Dminin2 <apnaeom22@gmail.com>
2025-08-25 20:59:18 +0900

    minishell.h: remove error.h

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: aomatsud <aomatsud@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/06 16:54:21 by aomatsud          #+#    #+#             */
-/*   Updated: 2025/08/23 14:22:44 by aomatsud         ###   ########.fr       */
+/*   Updated: 2025/08/25 20:53:44 by aomatsud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@
 # include "types.h"
 # include "utils.h"
 # include <errno.h>
-# include <error.h>
 # include <readline/history.h>
 # include <readline/readline.h>
 # include <stdio.h>
