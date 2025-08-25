3c81d5a2b4f4507f705b1bc70daca9d78706399a
Dminin2 <apnaeom22@gmail.com>
2025-08-25 19:42:28 +0900

    utils: add free_token and assert_error

--- srcs/utils/free.c
+++ srcs/utils/free.c
@@ -6,7 +6,7 @@
 /*   By: aomatsud <aomatsud@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/16 16:17:00 by aomatsud          #+#    #+#             */
-/*   Updated: 2025/08/18 09:09:25 by aomatsud         ###   ########.fr       */
+/*   Updated: 2025/08/22 21:46:49 by aomatsud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,5 +37,15 @@ void	free_cmd(t_cmd *cmd)
 		if (cmd->path)
 			free(cmd->path);
 		free(cmd);
+	}
+}
+
+void	free_token(t_token *tok)
+{
+	if (tok)
+	{
+		if (tok->value)
+			free(tok->value);
+		free(tok);
 	}
 }
