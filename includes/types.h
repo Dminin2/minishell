1c412cd12e34a9eb9c4fda89e4380aeaf58ba9eb
Dminin2 <apnaeom22@gmail.com>
2025-08-25 19:40:15 +0900

    types.h: add lexer, token, and tok_types

--- includes/types.h
+++ includes/types.h
@@ -6,25 +6,51 @@
 /*   By: aomatsud <aomatsud@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/15 17:21:17 by aomatsud          #+#    #+#             */
-/*   Updated: 2025/08/19 16:34:24 by aomatsud         ###   ########.fr       */
+/*   Updated: 2025/08/25 19:39:02 by aomatsud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TYPES_H
 # define TYPES_H
 
+# include "libft.h"
+
 typedef struct s_cmd
 {
-	char	*path;
-	char	**args;
-}			t_cmd;
+	char		*path;
+	char		**args;
+}				t_cmd;
+
+typedef enum e_tok_types
+{
+	TK_WORD,
+	TK_PIPE,
+	TK_REDIR_IN,
+	TK_REDIR_OUT,
+	TK_HEREDOC,
+	TK_APPEND,
+	TK_EOF
+}				t_tok_types;
+
+typedef struct s_token
+{
+	t_tok_types	type;
+	char		*value;
+}				t_token;
+
+typedef struct s_lexer
+{
+	const char	*line;
+	int			pos;
+}				t_lexer;
 
 typedef enum e_status
 {
 	SUCCESS,
 	ERR_SYSTEM,
+	ERR_SYNTAX,
 	ERR_CMD_NOT_FOUND,
 	ERR_NOT_VALID_PATH
-}			t_status;
+}				t_status;
 
 #endif
