08ac5a8c77a430a07a209feb1883fb951a950677
Dminin2 <apnaeom22@gmail.com>
2025-08-25 22:17:52 +0900

    free.c: add free_token_wrapper

--- srcs/utils/free.c
+++ srcs/utils/free.c
@@ -6,7 +6,7 @@
 /*   By: aomatsud <aomatsud@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/16 16:17:00 by aomatsud          #+#    #+#             */
-/*   Updated: 2025/08/22 21:46:49 by aomatsud         ###   ########.fr       */
+/*   Updated: 2025/08/25 22:13:35 by aomatsud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,11 @@ void	free_cmd(t_cmd *cmd)
 			free(cmd->path);
 		free(cmd);
 	}
+}
+
+void	free_token_wrapper(void *tok)
+{
+	free_token((t_token *)tok);
 }
 
 void	free_token(t_token *tok)
