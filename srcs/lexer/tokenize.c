3da954deab52f849e6243c8b3074f6f2ef4ab53b
Dminin2 <apnaeom22@gmail.com>
2025-08-27 18:06:04 +0900

    lexer: fix leaks and add detail

--- srcs/lexer/tokenize.c
+++ srcs/lexer/tokenize.c
@@ -6,7 +6,7 @@
 /*   By: aomatsud <aomatsud@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/22 11:32:59 by aomatsud          #+#    #+#             */
-/*   Updated: 2025/08/25 20:57:56 by aomatsud         ###   ########.fr       */
+/*   Updated: 2025/08/27 18:03:16 by aomatsud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,11 @@ int	is_operator(t_lexer *lex)
 	char	*op[] = {"|", "<<", ">>", "<", ">"};
 	int		i;
 
+	// op配列の中でも二文字あるredirectionを前に持っておいておくのに注意
 	i = 0;
 	while (i < 5)
 	{
+		// op[i]とlex->line[lex->pos]から一文字または二文字を確認
 		if (!ft_memcmp(op[i], &(lex->line[lex->pos]), ft_strlen(op[i])))
 			return (1);
 		i++;
@@ -33,6 +35,7 @@ int	is_operator(t_lexer *lex)
 	return (0);
 }
 
+//特別処理をするmetacharacterであるかどうか確認する。
 int	is_metacharacter(char c)
 {
 	if (ft_strchr("|<>", c))
@@ -49,8 +52,10 @@ t_status	handle_operator(t_lexer *lex, t_list **head)
 	tok = ft_calloc(sizeof(t_token), 1);
 	if (!tok)
 		return (ERR_SYSTEM);
+	//一文字めの確認
 	if (lex->line[lex->pos] == '<')
 	{
+		//二文字めの確認
 		if (lex->line[lex->pos + 1] == '<')
 		{
 			tok->type = TK_HEREDOC;
@@ -62,8 +67,10 @@ t_status	handle_operator(t_lexer *lex, t_list **head)
 			lex->pos += 1;
 		}
 	}
+	//一文字めの確認
 	else if (lex->line[lex->pos] == '>')
 	{
+		//二文字めの確認
 		if (lex->line[lex->pos + 1] == '>')
 		{
 			tok->type = TK_APPEND;
@@ -75,14 +82,20 @@ t_status	handle_operator(t_lexer *lex, t_list **head)
 			lex->pos += 1;
 		}
 	}
+	// redirectじゃなかったらパイプ
 	else
 	{
 		tok->type = TK_PIPE;
 		lex->pos += 1;
 	}
+	// lstに繋げるtokを内蔵した新しいt_listを作る
 	new = ft_lstnew(tok);
 	if (!new)
+	{
+		free(tok);
 		return (ERR_SYSTEM);
+	}
+	//作ったnew listを後ろに繋げる
 	ft_lstadd_back(head, new);
 	return (SUCCESS);
 }
@@ -93,20 +106,22 @@ t_status	handle_word(t_lexer *lex, t_list **head)
 	t_list	*new;
 	int		start;
 
+	//文字列の複製を作りたいので、startの位置を記録しておく
 	start = lex->pos;
-	tok = ft_calloc(sizeof(t_token), 1);
-	if (!tok)
-		return (ERR_SYSTEM);
+	// spaceにあたるまで繰り返す
 	while (lex->line[lex->pos] && !isspace(lex->line[lex->pos]))
 	{
+		// single quoteの中はmetacharacterがきてもdouble quoteがきても読み飛ばす
 		if (lex->line[lex->pos] == '\'')
 		{
 			lex->pos++;
 			while (lex->line[lex->pos] && lex->line[lex->pos] != '\'')
 				lex->pos++;
+			// single quoteが見える前に終端にきたらエラー
 			if (!lex->line[lex->pos])
 				return (ERR_SYNTAX);
 		}
+		// double quoteも同じことをやる
 		else if (lex->line[lex->pos] == '\"')
 		{
 			lex->pos++;
@@ -115,17 +130,30 @@ t_status	handle_word(t_lexer *lex, t_list **head)
 			if (!lex->line[lex->pos])
 				return (ERR_SYNTAX);
 		}
+		// metacharacterは特別扱いなので、そこで文字列が切れる
 		else if (is_metacharacter(lex->line[lex->pos]))
 			break ;
 		lex->pos++;
 	}
+	tok = ft_calloc(sizeof(t_token), 1);
+	if (!tok)
+		return (ERR_SYSTEM);
 	tok->type = TK_WORD;
+	// lex->line[start]からlex->line[lex->pos]までを複製する
 	tok->value = ft_substr(lex->line, start, lex->pos - start);
 	if (!tok->value)
+	{
+		free(tok);
 		return (ERR_SYSTEM);
+	}
+	// tokを内蔵した新しいt_listをつくる
 	new = ft_lstnew(tok);
 	if (!new)
+	{
+		free_token(tok);
 		return (ERR_SYSTEM);
+	}
+	// new listを連結する
 	ft_lstadd_back(head, new);
 	return (SUCCESS);
 }
@@ -136,18 +164,26 @@ t_list	*tokenize(char *line)
 	t_lexer		lex;
 	t_status	status;
 
+	// ft_lst系を使いたかったのでlst使ってるけど、malloc倍に増えるからなくてもよし
 	head = NULL;
+	// lexはreadlineで読み込んだlineを保存するのと、どこまで読んだかposに保存しておくやつ
 	lex.line = line;
 	lex.pos = 0;
+	// malloc失敗ならERR_SYSTEM,quoteが閉じていないならERR_SYNTAXで返ってくる
 	status = SUCCESS;
+	//文字列終端まで読み込む
 	while (lex.line[lex.pos])
 	{
+		// spaceならそれをスッキプする
 		if (isspace(lex.line[lex.pos]))
 			consume_blank(&lex);
+		// operator(pipe,redirection)ならその情報をlstに繋げる
 		else if (is_operator(&lex))
 			status = handle_operator(&lex, &head);
+		//それ以外ならwordとしてそのまま読み込みlstに繋げる
 		else
 			status = handle_word(&lex, &head);
+		// statusを確認してエラーがあったらエラーメッセージを表示させ、その時点でmainのループに戻る
 		if (status != SUCCESS)
 		{
 			if (status == ERR_SYSTEM)
