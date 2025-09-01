382ad117f46b5c934595dc84eeb216bca75da0d6
Dminin2 <apnaeom22@gmail.com>
2025-09-02 00:18:15 +0900

    utils.h: add free_pipeline

--- includes/utils.h
+++ includes/utils.h
@@ -6,7 +6,7 @@
 /*   By: aomatsud <aomatsud@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/16 16:18:32 by aomatsud          #+#    #+#             */
-/*   Updated: 2025/08/25 22:14:29 by aomatsud         ###   ########.fr       */
+/*   Updated: 2025/08/31 23:28:30 by aomatsud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,16 @@
 # include "types.h"
 
 void	free_args(char **args);
-void	free_cmd(t_cmd *cmd);
 void	free_token_wrapper(void *tok);
 void	free_token(t_token *tok);
+void	free_redir(t_redir *redir);
+void	free_cmd(t_cmd *cmd);
+void	free_cmd_wrapper(void *cmd);
+void	free_pipeline(t_pipeline *pipeline);
 
 void	print_error_msg(char *context, t_status status);
 void	assert_error(t_list *lst, char *context, t_status status);
+void	assert_error_parser(t_list *lst, char *context, t_status status);
 void	exit_error(t_cmd *cmd, char *context, t_status status, int exit_status);
 
 #endif
