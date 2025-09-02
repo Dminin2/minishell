38c4fba39a66fb12f925afdffaceb194e3c5a624
Dminin2 <apnaeom22@gmail.com>
2025-09-02 21:07:01 +0900

    debug.h: add debug

---
+++ includes/debug.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   debug.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aomatsud <aomatsud@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/02 20:59:45 by aomatsud          #+#    #+#             */
+/*   Updated: 2025/09/02 21:00:48 by aomatsud         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DEBUG_H
+# define DEBUG_H
+# include "types.h"
+
+void	print_token(t_list *head);
+void	print_pipeline(t_pipeline *pipeline);
+
+#endif
