99a5a0937184ec9716032d51e77938a78fb5f417
Dminin2 <apnaeom22@gmail.com>
2025-09-02 17:55:20 +0900

    parser.h: add parse_*.c

--- includes/parser.h
+++ includes/parser.h
@@ -6,7 +6,7 @@
 /*   By: aomatsud <aomatsud@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/31 20:37:20 by aomatsud          #+#    #+#             */
-/*   Updated: 2025/08/31 20:37:47 by aomatsud         ###   ########.fr       */
+/*   Updated: 2025/09/02 17:41:55 by aomatsud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,10 @@
 # include "minishell.h"
 
 t_pipeline	*parse(t_list *tok_lst);
+void		handle_error(t_list *tok_lst, t_list *head, t_status status);
+t_status	get_redirection(t_list **tok_lst, t_cmd *cmd);
+t_status	get_simple_command(t_list **tok_lst, t_cmd *cmd);
+
+t_status	add_newlst(t_list **head, void *content);
 
 #endif
