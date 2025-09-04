4c602cc217aa16cd5ef436e4b5e066a96003b181
Dminin2 <apnaeom22@gmail.com>
2025-09-04 21:59:33 +0900

    t_pipelineから参照できるようにしたのと、redirectionを追加した

--- includes/execute.h
+++ includes/execute.h
@@ -6,7 +6,7 @@
 /*   By: aomatsud <aomatsud@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/16 01:11:45 by aomatsud          #+#    #+#             */
-/*   Updated: 2025/08/19 16:35:49 by aomatsud         ###   ########.fr       */
+/*   Updated: 2025/09/04 20:18:04 by aomatsud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 
 # include "types.h"
 
-void		execute(t_cmd *cmd, char **envp);
-void		child_process(t_cmd *cmd, char **envp);
+void		execute(t_pipeline *pipeline, char **envp);
+void		child_process(t_pipeline *pipeline, char **envp);
 
 t_status	resolve_command_path(t_cmd *cmd, char **envp);
 
