81123f4a2143207f63098894b115d9f9bcd4957c
Dminin2 <apnaeom22@gmail.com>
2025-09-04 22:04:23 +0900

    heredoc用のtmpfileをcloseする関数を追加

---
+++ srcs/utils/close.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   close.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aomatsud <aomatsud@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/04 20:27:22 by aomatsud          #+#    #+#             */
+/*   Updated: 2025/09/04 20:35:54 by aomatsud         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	loop_redir_lst(t_list *redir_lst)
+{
+	t_redir	*redir;
+
+	while (redir_lst)
+	{
+		redir = redir_lst->content;
+		if (redir->fd_hd > 0)
+			close(redir->fd_hd);
+		redir_lst = redir_lst->next;
+	}
+}
+
+void	close_heredoc(t_list *cmd_lst)
+{
+	t_cmd	*cmd;
+	while (cmd_lst)
+	{
+		cmd = cmd_lst->content;
+		if (cmd->redir_lst)
+			loop_redir_lst(cmd->redir_lst);
+		cmd_lst = cmd_lst->next;
+	}
+}
