3300a0e946807fc73a0c1fb056090e93a4e81638
Dminin2 <apnaeom22@gmail.com>
2025-09-10 16:11:44 +0900

    pipeを作成し、コマンドの数分forkするように修正

--- srcs/executor/process.c
+++ srcs/executor/process.c
@@ -6,30 +6,98 @@
 /*   By: aomatsud <aomatsud@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/16 23:42:22 by aomatsud          #+#    #+#             */
-/*   Updated: 2025/09/04 20:36:09 by aomatsud         ###   ########.fr       */
+/*   Updated: 2025/09/09 20:32:44 by aomatsud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	wait_child(t_pipeline *pipeline, pid_t pid)
+void	wait_child(t_pipeline *pipeline, pid_t *pids)
 {
+	int	i;
 	int	status;
 
+	i = 0;
+	close_pipes(pipeline->pipes, pipeline->n - 1);
 	close_heredoc(pipeline->cmd_lst);
+	while (i < pipeline->n)
+	{
+		waitpid(pids[i], &status, 0);
+		i++;
+	}
+	free(pids);
 	free_pipeline(pipeline);
-	waitpid(pid, &status, 0);
+}
+
+void	wait_child_fork_pos(t_pipeline *pipeline, pid_t *pids, int pos)
+{
+	int	i;
+	int	status;
+
+	i = 0;
+	close_pipes(pipeline->pipes, pipeline->n - 1);
+	close_heredoc(pipeline->cmd_lst);
+	while (i < pos)
+	{
+		waitpid(pids[i], &status, 0);
+		i++;
+	}
+	free(pids);
+}
+
+int	fork_all_children(t_pipeline *pipeline, pid_t *pids, char **envp)
+{
+	int	i;
+
+	i = 0;
+	while (i < pipeline->n)
+	{
+		pids[i] = fork();
+		if (pids[i] < 0)
+		{
+			free(pids);
+			return (i);
+		}
+		if (pids[i] == 0)
+		{
+			free(pids);
+			execute(pipeline, i, envp);
+		}
+		i++;
+	}
+	return (0);
 }
 
 void	child_process(t_pipeline *pipeline, char **envp)
 {
-	pid_t	pid;
+	t_status	status;
+	pid_t		*pids;
+	int			fork_pos;
 
-	pid = fork();
-	if (pid < 0)
-		exit_error(pipeline, "fork", ERR_SYSTEM, EXIT_FAILURE);
-	else if (pid == 0)
-		execute(pipeline, envp);
-	else
-		wait_child(pipeline, pid);
+	status = create_pipes(pipeline);
+	if (status != SUCCESS)
+	{
+		assert_error_parent(pipeline, "malloc", ERR_SYSTEM);
+		return ;
+	}
+	status = pipe_pipes(pipeline->pipes, pipeline->n - 1);
+	if (status != SUCCESS)
+	{
+		assert_error_parent(pipeline, "pipe", ERR_SYSTEM);
+		return ;
+	}
+	pids = ft_calloc(pipeline->n, sizeof(pid_t));
+	if (!pids)
+	{
+		assert_error_parent(pipeline, "malloc", ERR_SYSTEM);
+		return ;
+	}
+	fork_pos = fork_all_children(pipeline, pids, envp);
+	if (fork_pos)
+	{
+		wait_child_fork_pos(pipeline, pids, fork_pos);
+		assert_error_parent(pipeline, "fork", ERR_SYSTEM);
+		return ;
+	}
+	wait_child(pipeline, pids);
 }
