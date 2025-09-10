5aedca7ee84d53fd676432ac97da92d7c32c5008
Dminin2 <apnaeom22@gmail.com>
2025-09-10 16:10:56 +0900

    pipe_duplicateを追加

--- srcs/executor/execute.c
+++ srcs/executor/execute.c
@@ -6,11 +6,29 @@
 /*   By: aomatsud <aomatsud@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/16 01:08:34 by aomatsud          #+#    #+#             */
-/*   Updated: 2025/09/04 20:24:10 by aomatsud         ###   ########.fr       */
+/*   Updated: 2025/09/09 20:36:02 by aomatsud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+t_cmd	*get_cmd_from_lst(t_list *head, int target)
+{
+	int		i;
+	t_list	*lst;
+
+	i = 0;
+	lst = head;
+	while (lst && i < target)
+	{
+		lst = lst->next;
+		i++;
+	}
+	if (!lst)
+		return (NULL);
+	else
+		return (lst->content);
+}
 
 void	handle_redir_err(t_pipeline *pipeline, t_redir_err err)
 {
@@ -20,13 +38,17 @@ void	handle_redir_err(t_pipeline *pipeline, t_redir_err err)
 		exit_error(pipeline, "dup", ERR_SYSTEM, EXIT_FAILURE);
 }
 
-void	execute(t_pipeline *pipeline, char **envp)
+void	execute(t_pipeline *pipeline, int pos, char **envp)
 {
 	t_status	status;
 	t_redir_err	err;
 	t_cmd		*cmd;
 
-	cmd = pipeline->cmd_lst->content;
+	cmd = get_cmd_from_lst(pipeline->cmd_lst, pos);
+	status = pipe_duplicate(pipeline, pos);
+	close_pipes(pipeline->pipes, pipeline->n - 1);
+	if (status != SUCCESS)
+		exit_error(pipeline, "dup", status, EXIT_FAILURE);
 	err.status = SUCCESS;
 	err.redir_err = NULL;
 	redirect(cmd->redir_lst, &err);
