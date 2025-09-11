917f8dd0e56d1cd0a3e483ee47321351049c262c
Dminin2 <apnaeom22@gmail.com>
2025-09-12 00:21:37 +0900

    debug関数をいい感じに修正

--- srcs/debug/print_pipeline.c
+++ srcs/debug/print_pipeline.c
@@ -6,7 +6,7 @@
 /*   By: aomatsud <aomatsud@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/02 20:58:01 by aomatsud          #+#    #+#             */
-/*   Updated: 2025/09/02 20:58:11 by aomatsud         ###   ########.fr       */
+/*   Updated: 2025/09/12 00:19:08 by aomatsud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,23 @@ void	print_redir_lst(t_list *head)
 	t_redir	*redir;
 	int		i;
 
-	printf("---redir list---\n");
+	printf("  Rdirections:\n");
 	i = 1;
 	while (head)
 	{
 		redir = head->content;
-		printf("<%d>\n", i);
-		printf("value : %s\n", redir->value);
+		printf("    [%d]\n", i);
+		printf("      value : %s\n", redir->value);
 		if (redir->type == R_IN)
-			printf("type : R_IN\n");
+			printf("      type  : R_IN\n");
 		else if (redir->type == R_OUT)
-			printf("type : R_OUT\n");
+			printf("      type  : R_OUT\n");
 		else if (redir->type == R_HEREDOC)
-			printf("type : R_HEREDOC\n");
+			printf("      type  : R_HEREDOC\n");
 		else if (redir->type == R_APPEND)
-			printf("type : R_APPEND\n");
+			printf("      type  : R_APPEND\n");
 		else
-			printf("type : null\n");
+			printf("      type  : null\n");
 		head = head->next;
 		i++;
 	}
@@ -46,32 +46,31 @@ void	print_cmd_lst(t_list *head)
 	t_cmd	*cmd;
 
 	i = 1;
-	printf("-------cmd list--------\n");
 	while (head)
 	{
-		printf("<<<%d>>>\n", i);
+		printf("\nCommand #%d\n", i);
 		cmd = head->content;
 		j = 0;
 		if (cmd->args)
 		{
+			printf("  Args:\n");
 			while (cmd->args[j])
 			{
-				printf("word(%s) ", cmd->args[j]);
+				printf("  -(%s)\n", cmd->args[j]);
 				j++;
 			}
 		}
-		printf("\n");
 		if (cmd->redir_lst)
 			print_redir_lst(cmd->redir_lst);
 		i++;
 		head = head->next;
-		if (head)
-			printf("\n");
 	}
 }
 
 void	print_pipeline(t_pipeline *pipeline)
 {
-	printf("cmds : %d\n", pipeline->n);
+	printf("=== pipeline ===\n");
+	printf("cmd count : %d\n", pipeline->n);
 	print_cmd_lst(pipeline->cmd_lst);
+	printf("\n===  result  ===\n");
 }
