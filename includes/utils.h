e357c5364e011f42eee8e2108ce18688b7b6eea9
Dminin2 <apnaeom22@gmail.com>
2025-09-16 12:10:13 +0900

    t_pipeline_irのfree関数とparserでのエラー処理関数を追加

--- includes/utils.h
+++ includes/utils.h
@@ -6,7 +6,7 @@
 /*   By: aomatsud <aomatsud@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/16 16:18:32 by aomatsud          #+#    #+#             */
-/*   Updated: 2025/09/09 18:58:22 by aomatsud         ###   ########.fr       */
+/*   Updated: 2025/09/14 00:20:34 by aomatsud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,21 @@
 # include "types.h"
 
 void	free_args(char **args);
+
 void	free_token_wrapper(void *tok);
 void	free_token(t_token *tok);
+
 void	free_redir(t_redir *redir);
 void	free_cmd(t_cmd *cmd);
 void	free_cmd_wrapper(void *cmd);
 void	free_pipes(int **pipes, int n);
 void	free_pipeline(t_pipeline *pipeline);
+
+void	free_str_wrapper(void *str);
+void	free_str(char *str);
+void	free_cmd_ir_wrapper(void *cmd_ir);
+void	free_cmd_ir(t_cmd_ir *cmd_ir);
+void	free_pipeline_ir(t_pipeline_ir *pipeline_ir);
 
 void	close_heredoc(t_list *cmd_lst);
 void	close_pipes(int **pipes, int n);
