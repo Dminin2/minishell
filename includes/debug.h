ecf9cb2cd0018fde747e261d223e5b3c27218a3f
Dminin2 <apnaeom22@gmail.com>
2025-09-22 10:51:43 +0900

    last_statusを出力するデバッグ用の関数を追加

--- includes/debug.h
+++ includes/debug.h
@@ -6,7 +6,7 @@
 /*   By: aomatsud <aomatsud@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/02 20:59:45 by aomatsud          #+#    #+#             */
-/*   Updated: 2025/09/16 16:31:48 by aomatsud         ###   ########.fr       */
+/*   Updated: 2025/09/22 10:47:54 by aomatsud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,6 @@ void	print_pipeline_ir(t_pipeline_ir *pipeline_ir);
 void	print_pipeline(t_pipeline *pipeline);
 void	print_redir_lst(t_list *head);
 void	print_env_lst(t_list *env_lst);
+void	print_status(int status);
 
 #endif
