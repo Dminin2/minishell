d7a2c0de343af850ada95e1c6f84c54e4c932e98
Dminin2 <apnaeom22@gmail.com>
2025-10-03 23:19:14 +0900

    ft_putendl_fdを修正

--- libft/srcs/libft/ft_putendl_fd.c
+++ libft/srcs/libft/ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: aomatsud <aomatsud@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/27 16:29:01 by aomatsud          #+#    #+#             */
-/*   Updated: 2025/09/05 13:50:00 by aomatsud         ###   ########.fr       */
+/*   Updated: 2025/10/03 23:18:38 by aomatsud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,5 @@ void	ft_putendl_fd(char *s, int fd)
 		return ;
 	len = ft_strlen(s);
 	write(fd, s, len);
-	write(1, "\n", 1);
+	write(fd, "\n", 1);
 }
