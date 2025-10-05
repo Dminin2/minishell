8b4a0974e0c1237a0b7d2243a9805302585ffb2f
Hayase Maruyama <mrym.124.hys@gmail.com>
2025-10-05 13:32:03 +0900

    #76 builtin_pwdの実装

--- srcs/builtins/builtin_pwd.c
+++ srcs/builtins/builtin_pwd.c
@@ -6,20 +6,44 @@
 /*   By: hmaruyam <hmaruyam@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/02 09:53:08 by hmaruyam          #+#    #+#             */
-/*   Updated: 2025/10/04 20:35:55 by hmaruyam         ###   ########.fr       */
+/*   Updated: 2025/10/05 13:30:59 by hmaruyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-t_status	builtin_pwd(int out_fd)
+int	is_pwd_valid(char *env_pwd)
+{
+	struct stat	stat_of_current;
+	struct stat	stat_of_pwd;
+
+	if (stat(".", &stat_of_current) == -1)
+		return (0);
+	if (stat(env_pwd, &stat_of_pwd) == -1)
+		return (0);
+	if (stat_of_current.st_ino == stat_of_pwd.st_ino)
+		return (1);
+	return (0);
+}
+
+int	builtin_pwd(t_minishell *minishell)
 {
 	char	*cwd;
+	char	*env_pwd;
 
-	cwd = getcwd(NULL, 0);
-	if (!cwd)
-		return (FAILURE);
-	ft_putendl_fd(cwd, out_fd);
-	free(cwd);
-	return (SUCCESS);
+	env_pwd = search_env(minishell->env_lst, "PWD");
+	if (env_pwd && is_pwd_valid(env_pwd))
+		ft_putendl_fd(env_pwd, STDOUT_FILENO);
+	else
+	{
+		cwd = getcwd(NULL, 0);
+		if (!cwd)
+		{
+			print_error_msg("getcwd", ERR_SYSTEM);
+			return (0);
+		}
+		ft_putendl_fd(cwd, STDOUT_FILENO);
+		free(cwd);
+	}
+	return (1);
 }
