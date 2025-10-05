c8bd8a9126d72f8b64a1f942e950515cea6be279
Dminin2 <apnaeom22@gmail.com>
2025-10-05 17:46:12 +0900

    builtin error用にt_blt_errorを追加

--- includes/builtin.h
+++ includes/builtin.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtin.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hmaruyam <hmaruyam@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: aomatsud <aomatsud@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/02 12:54:35 by hmaruyam          #+#    #+#             */
-/*   Updated: 2025/10/05 14:58:08 by hmaruyam         ###   ########.fr       */
+/*   Updated: 2025/10/05 17:44:04 by aomatsud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,11 @@
 # include "common.h"
 # include <stdlib.h>
 # include <unistd.h>
+
+typedef enum e_blt_error
+{
+	BLTERR_ERRNO
+}	t_blt_error;
 
 int	builtin_pwd(t_minishell *minishell);
 
