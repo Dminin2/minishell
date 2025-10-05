f2ce844f5cf623317cc131a96a8d0bfe781ca457
Dminin2 <apnaeom22@gmail.com>
2025-10-05 20:33:10 +0900

    最後の子プロセスの終了ステータスを保持できるようにした

--- srcs/executor/process.c
+++ srcs/executor/process.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   process.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hmaruyam <hmaruyam@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: aomatsud <aomatsud@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/16 23:42:22 by aomatsud          #+#    #+#             */
-/*   Updated: 2025/10/05 09:44:56 by hmaruyam         ###   ########.fr       */
+/*   Updated: 2025/10/05 20:14:57 by aomatsud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	wait_child(t_pipeline *pipeline, pid_t *pids)
+void	wait_child(t_minishell *minishell, t_pipeline *pipeline, pid_t *pids)
 {
 	int	i;
 	int	status;
@@ -22,14 +22,25 @@ void	wait_child(t_pipeline *pipeline, pid_t *pids)
 	close_heredoc(pipeline->cmd_lst);
 	while (i < pipeline->n)
 	{
-		waitpid(pids[i], &status, 0);
+		if (waitpid(pids[i], &status, 0) == -1)
+		{
+			if (errno == EINTR)
+				continue ;
+			else
+				print_error_msg("waitpid", ERR_WAITPID);
+		}
 		i++;
 	}
+	if (WIFEXITED(status))
+		minishell->last_status = WEXITSTATUS(status);
+	else if (WIFSIGNALED(status))
+		minishell->last_status = 128 + WTERMSIG(status);
 	free(pids);
 	free_pipeline(pipeline);
 }
 
-void	wait_child_fork_pos(t_pipeline *pipeline, pid_t *pids, int pos)
+void	wait_child_fork_pos(t_minishell *minishell, t_pipeline *pipeline,
+		pid_t *pids, int pos)
 {
 	int	i;
 	int	status;
@@ -39,9 +50,19 @@ void	wait_child_fork_pos(t_pipeline *pipeline, pid_t *pids, int pos)
 	close_heredoc(pipeline->cmd_lst);
 	while (i < pos)
 	{
-		waitpid(pids[i], &status, 0);
+		if (waitpid(pids[i], &status, 0) == -1)
+		{
+			if (errno == EINTR)
+				continue ;
+			else
+				print_error_msg("waitpid", ERR_WAITPID);
+		}
 		i++;
 	}
+	if (WIFEXITED(status))
+		minishell->last_status = WEXITSTATUS(status);
+	else if (WIFSIGNALED(status))
+		minishell->last_status = 128 + WTERMSIG(status);
 	free(pids);
 }
 
@@ -92,9 +113,9 @@ void	child_process(t_minishell *minishell, t_pipeline *pipeline)
 	fork_pos = fork_all_children(minishell, pipeline, pids);
 	if (fork_pos)
 	{
-		wait_child_fork_pos(pipeline, pids, fork_pos);
+		wait_child_fork_pos(minishell, pipeline, pids, fork_pos);
 		assert_error_parent(pipeline, "fork", ERR_SYSTEM);
 		return ;
 	}
-	wait_child(pipeline, pids);
+	wait_child(minishell, pipeline, pids);
 }
