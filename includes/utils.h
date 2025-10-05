b353c361c38f33dbe7bdc07a5c6899810604f12a
Hayase Maruyama <mrym.124.hys@gmail.com>
2025-10-05 21:55:47 +0900

    fix:utilsに前方宣言を追加

--- includes/utils.h
+++ includes/utils.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aomatsud <aomatsud@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: hmaruyam <hmaruyam@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/16 16:18:32 by aomatsud          #+#    #+#             */
-/*   Updated: 2025/10/05 21:01:51 by aomatsud         ###   ########.fr       */
+/*   Updated: 2025/10/05 21:55:29 by hmaruyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@
 # include <unistd.h>
 
 typedef enum e_blt_error	t_blt_error;
+typedef struct s_pipeline	t_pipeline;
 
 void						free_args(char **args);
 void						free_str_wrapper(void *str);
