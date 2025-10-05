63d3414d77af492c7e3d8759f15acd7bd776e4a7
Dminin2 <apnaeom22@gmail.com>
2025-10-05 17:50:08 +0900

    print_error_msg_builtinを追加

--- includes/utils.h
+++ includes/utils.h
@@ -3,16 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   utils.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hmaruyam <hmaruyam@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: aomatsud <aomatsud@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/16 16:18:32 by aomatsud          #+#    #+#             */
-/*   Updated: 2025/10/03 11:46:30 by hmaruyam         ###   ########.fr       */
+/*   Updated: 2025/10/05 17:49:40 by aomatsud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef UTILS_H
 # define UTILS_H
 
+# include "builtin.h"
 # include "common.h"
 # include <errno.h>
 # include <stdlib.h>
@@ -28,12 +29,14 @@ void		close_pipes(int **pipes, int n);
 
 t_status	add_newlst(t_list **head, void *content);
 
-void	print_error_msg(char *context, t_status status);
-void	assert_error_lst(t_list *lst, char *context, t_status status,
-			void (*del)(void *));
-void	assert_error_parent(t_pipeline *pipeline, char *context,
-			t_status status);
-void	exit_error(t_pipeline *pipeline, char *context, t_status status,
-			int exit_status);
+void		print_error_msg(char *context, t_status status);
+void		print_error_msg_builtin(char *cmd, char *context,
+				t_blt_error error);
+void		assert_error_lst(t_list *lst, char *context, t_status status,
+				void (*del)(void *));
+void		assert_error_parent(t_pipeline *pipeline, char *context,
+				t_status status);
+void		exit_error(t_pipeline *pipeline, char *context, t_status status,
+				int exit_status);
 
 #endif
