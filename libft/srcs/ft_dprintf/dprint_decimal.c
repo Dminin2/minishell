33a2afcb686ab4722c5fdc306604cb14fd90ae63
Dminin2 <apnaeom22@gmail.com>
2025-10-16 12:57:27 +0900

    関数をrename

--- libft/srcs/ft_dprintf/dprint_decimal.c
+++ libft/srcs/ft_dprintf/dprint_decimal.c
@@ -6,19 +6,19 @@
 /*   By: aomatsud <aomatsud@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/03 11:33:35 by aomatsud          #+#    #+#             */
-/*   Updated: 2025/10/16 00:53:47 by aomatsud         ###   ########.fr       */
+/*   Updated: 2025/10/16 12:56:59 by aomatsud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_dprintf.h"
 
-static int	ft_dputnbr_fd_unsigned_error(unsigned int n, int fd)
+static int	ft_dputnbr_unsigned_fd_error(unsigned int n, int fd)
 {
 	char	c;
 
 	if (n >= 10)
 	{
-		if (ft_dputnbr_fd_unsigned_error(n / 10, fd) == -1)
+		if (ft_dputnbr_unsigned_fd_error(n / 10, fd) == -1)
 			return (-1);
 	}
 	c = n % 10 + '0';
@@ -73,7 +73,7 @@ int	dprint_decimal(int fd, int nbr)
 
 int	dprint_unsigned_decimal(int fd, unsigned int nbr)
 {
-	if (ft_dputnbr_fd_unsigned_error(nbr, fd) == -1)
+	if (ft_dputnbr_unsigned_fd_error(nbr, fd) == -1)
 		return (-1);
 	return (count_unsigned_decimal_digits(nbr));
 }
