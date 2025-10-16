c7863cb32f8a696966b64804e23308c167f451a9
Hayase Maruyama <mrym.124.hys@gmail.com>
2025-10-17 00:00:01 +0900

    #206 pack_envのnormを修正

--- srcs/env/pack_env.c
+++ srcs/env/pack_env.c
@@ -6,13 +6,13 @@
 /*   By: aomatsud <aomatsud@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/04 02:14:31 by aomatsud          #+#    #+#             */
-/*   Updated: 2025/10/13 16:19:47 by aomatsud         ###   ########.fr       */
+/*   Updated: 2025/10/16 00:00:00 by hmaruyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	count_env_lst(t_list *env_lst)
+static int	count_env_lst(t_list *env_lst)
 {
 	int		n;
 	t_env	*env;
@@ -28,17 +28,11 @@ int	count_env_lst(t_list *env_lst)
 	return (n);
 }
 
-char	**pack_env(t_list *env_lst)
+static char	**create_env_array(t_list *env_lst, char **envp)
 {
-	char	**envp;
-	int		env_count;
-	t_env	*env;
 	int		i;
+	t_env	*env;
 
-	env_count = count_env_lst(env_lst);
-	envp = ft_calloc(env_count + 1, sizeof(char *));
-	if (!envp)
-		return (NULL);
 	i = 0;
 	while (env_lst)
 	{
@@ -56,4 +50,16 @@ char	**pack_env(t_list *env_lst)
 		env_lst = env_lst->next;
 	}
 	return (envp);
+}
+
+char	**pack_env(t_list *env_lst)
+{
+	char	**envp;
+	int		env_count;
+
+	env_count = count_env_lst(env_lst);
+	envp = ft_calloc(env_count + 1, sizeof(char *));
+	if (!envp)
+		return (NULL);
+	return (create_env_array(env_lst, envp));
 }
