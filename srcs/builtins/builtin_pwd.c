c020bb7ca81649631361b4b5829ded65a32c18e8
Hayase Maruyama <mrym.124.hys@gmail.com>
2025-10-25 17:54:22 +0900

    Revert "#216 is_pwd_validをutilsに変更"
    This reverts commit 4ba9f0791c8e39585c52300972e1853acea18339.

--- srcs/builtins/builtin_pwd.c
+++ srcs/builtins/builtin_pwd.c
@@ -6,11 +6,25 @@
 /*   By: hmaruyam <hmaruyam@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/02 09:53:08 by hmaruyam          #+#    #+#             */
-/*   Updated: 2025/10/22 16:35:00 by hmaruyam         ###   ########.fr       */
+/*   Updated: 2025/10/08 22:54:49 by hmaruyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static int	is_pwd_valid(char *env_pwd)
+{
+	struct stat	stat_of_current;
+	struct stat	stat_of_pwd;
+
+	if (stat(".", &stat_of_current) == -1)
+		return (0);
+	if (stat(env_pwd, &stat_of_pwd) == -1)
+		return (0);
+	if (stat_of_current.st_ino == stat_of_pwd.st_ino)
+		return (1);
+	return (0);
+}
 
 int	builtin_pwd(t_minishell *minishell)
 {
@@ -18,7 +32,7 @@ int	builtin_pwd(t_minishell *minishell)
 	char	*env_pwd;
 
 	env_pwd = search_env(minishell->env_lst, "PWD");
-	if (env_pwd)
+	if (env_pwd && is_pwd_valid(env_pwd))
 		ft_printf("%s\n", env_pwd);
 	else
 	{
