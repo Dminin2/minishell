2458e4091b469103ca553fb91ad07bfbb56f29e6
Hayase Maruyama <mrym.124.hys@gmail.com>
2025-11-02 19:12:37 +0900

    #270 コンパイルエラーを解消

--- includes/signals.h
+++ includes/signals.h
@@ -6,7 +6,7 @@
 /*   By: hmaruyam <hmaruyam@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 02:45:41 by aomatsud          #+#    #+#             */
-/*   Updated: 2025/11/02 19:08:32 by hmaruyam         ###   ########.fr       */
+/*   Updated: 2025/11/02 19:12:18 by hmaruyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,10 @@
 # include <signal.h>
 
 extern volatile sig_atomic_t	g_sig;
+
+void							sigint_handler_in_heredoc(int signo);
+void							sigint_handler_in_readline(int signo);
+void							set_action(int sig, void (*handler)(int));
 
 void							set_signal_interactive(void);
 void							set_signal_noninteractive(void);
