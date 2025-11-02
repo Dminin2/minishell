91098d347043362aeed8f0aee8f20e498d143c66
Hayase Maruyama <mrym.124.hys@gmail.com>
2025-11-03 01:20:12 +0900

    #272 ヘルパー関数とstatusロジック変えてnorm解消

--- srcs/expander/expand.c
+++ srcs/expander/expand.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   expand.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aomatsud <aomatsud@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: hmaruyam <hmaruyam@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/22 22:14:28 by aomatsud          #+#    #+#             */
-/*   Updated: 2025/10/15 23:17:19 by aomatsud         ###   ########.fr       */
+/*   Updated: 2025/11/03 01:17:46 by hmaruyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,16 @@ t_status	expand_and_convert_args(t_minishell *minishell, t_list *args_lst,
 	return (status);
 }
 
+static t_list	*handle_cmd_error(t_minishell *minishell, t_list **head,
+		t_cmd *cmd)
+{
+	if (cmd)
+		free_cmd(cmd);
+	minishell->last_status = error_lst(*head, "malloc", ERR_MALLOC,
+			free_cmd_wrapper);
+	return (NULL);
+}
+
 t_list	*get_cmd_lst(t_minishell *minishell, t_list *cmd_ir_lst)
 {
 	t_cmd		*cmd;
@@ -32,37 +42,22 @@ t_list	*get_cmd_lst(t_minishell *minishell, t_list *cmd_ir_lst)
 	t_cmd_ir	*cmd_ir;
 
 	head = NULL;
+	status = SUCCESS;
 	while (cmd_ir_lst)
 	{
 		cmd_ir = cmd_ir_lst->content;
 		cmd = ft_calloc(1, sizeof(t_cmd));
 		if (!cmd)
-		{
-			minishell->last_status = error_lst(head, "malloc", ERR_MALLOC,
-					free_cmd_wrapper);
-			return (NULL);
-		}
+			return (handle_cmd_error(minishell, &head, cmd));
 		if (cmd_ir->args_lst)
-		{
 			status = expand_and_convert_args(minishell, cmd_ir->args_lst, cmd);
-			if (status != SUCCESS)
-			{
-				free_cmd(cmd);
-				minishell->last_status = error_lst(head, "malloc", ERR_MALLOC,
-						free_cmd_wrapper);
-				return (NULL);
-			}
-		}
+		if (status != SUCCESS)
+			return (handle_cmd_error(minishell, &head, cmd));
 		cmd->redir_lst = cmd_ir->redir_lst;
 		cmd_ir->redir_lst = NULL;
 		status = add_newlst(&head, cmd);
 		if (status != SUCCESS)
-		{
-			free_cmd(cmd);
-			minishell->last_status = error_lst(head, "malloc", ERR_MALLOC,
-					free_cmd_wrapper);
-			return (NULL);
-		}
+			return (handle_cmd_error(minishell, &head, cmd));
 		cmd_ir_lst = cmd_ir_lst->next;
 	}
 	return (head);
