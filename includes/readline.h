a430050908b4bc88e45f0f1a529142f8a1afa818
Hayase Maruyama <mrym.124.hys@gmail.com>
2025-11-04 22:50:57 +0900

    #299 色のマクロやめてベタ書きに修正

--- includes/readline.h
+++ includes/readline.h
@@ -6,7 +6,7 @@
 /*   By: hmaruyam <hmaruyam@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/15 17:17:56 by aomatsud          #+#    #+#             */
-/*   Updated: 2025/11/04 22:00:41 by hmaruyam         ###   ########.fr       */
+/*   Updated: 2025/11/04 22:50:35 by hmaruyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,8 @@
 # include <stdio.h>
 # include <unistd.h>
 
-# define C_RESET "\033[0m"
-# define C_GREEN "\033[32m"
-# define C_RED "\033[31m"
-# define C_BLUE "\033[34m"
-# define C_BOLD "\033[1m"
-# define PROMPT_PART1_SUCCESS C_GREEN "minishell" C_RESET " ➜ " C_BLUE C_BOLD
-# define PROMPT_PART1_ERROR C_GREEN "minishell" C_RESET " " C_RED "➜" C_RESET " " C_BLUE C_BOLD
-# define PROMPT_PART2 C_RESET " $ "
-# define PROMPT_PART1 "\033[32mminishell\033[0m ➜ \033[34m\033[1m"
+# define PROMPT_PART1_SUCCESS "\033[32mminishell\033[0m ➜ \033[34m\033[1m"
+# define PROMPT_PART1_ERROR "\033[32mminishell\033[0m \033[31m➜\033[0m \033[34m\033[1m"
 # define PROMPT_PART2 "\033[0m $ "
 
 typedef struct s_input
