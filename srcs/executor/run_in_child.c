77f350c14a14bd54af9b7913e9f43e3c6d8fe64a
Hayase Maruyama <mrym.124.hys@gmail.com>
2025-11-07 01:08:30 +0900

    #312 run_in_childのnormを解消

--- srcs/executor/run_in_child.c
+++ srcs/executor/run_in_child.c
@@ -6,7 +6,7 @@
 /*   By: hmaruyam <hmaruyam@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/16 01:08:34 by aomatsud          #+#    #+#             */
-/*   Updated: 2025/11/07 00:57:43 by hmaruyam         ###   ########.fr       */
+/*   Updated: 2025/11/07 01:08:10 by hmaruyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,38 +30,12 @@ t_cmd	*get_cmd_from_lst(t_list *head, int target)
 		return (lst->content);
 }
 
-void	run_in_child(t_minishell *minishell, t_pipeline *pipeline, int pos)
+static void	execute_external_command(t_minishell *minishell,
+		t_pipeline *pipeline, t_cmd *cmd)
 {
-	t_status		status;
-	t_redir_err		err;
-	t_cmd			*cmd;
-	t_command_type	type;
-	char			**envp;
+	t_status	status;
+	char		**envp;
 
-	cmd = get_cmd_from_lst(pipeline->cmd_lst, pos);
-	status = pipe_duplicate(pipeline, pos);
-	close_pipes(pipeline->pipes, pipeline->n - 1);
-	if (status != SUCCESS)
-		exit_error(minishell, pipeline, "dup", status);
-	err.status = SUCCESS;
-	err.redir_err = NULL;
-	redirect(minishell, cmd->redir_lst, &err);
-	if (err.status != SUCCESS)
-		handle_redir_error(minishell, pipeline, err);
-	type = scan_command_type(cmd);
-	status = set_underscore_for_invocation(minishell, cmd, type);
-	if (status != SUCCESS)
-		exit_error(minishell, pipeline, "malloc", ERR_MALLOC);
-	if (type != EXTERNAL && type != NO_CMD)
-	{
-		execute_builtin(minishell, cmd, type, pipeline->n);
-		free_pipeline(pipeline);
-		ft_lstclear(&(minishell->env_lst), free_env_wrapper);
-		free(minishell->cwd);
-		exit(minishell->last_status);
-	}
-	else if (type == NO_CMD)
-		exit_success(minishell, pipeline);
 	status = resolve_command_path(cmd, minishell->env_lst);
 	if (status != SUCCESS)
 	{
@@ -78,4 +52,51 @@ void	run_in_child(t_minishell *minishell, t_pipeline *pipeline, int pos)
 		free_args(envp);
 		handle_execve_error(minishell, pipeline, cmd);
 	}
+}
+
+static void	execute_builtin_in_child(t_minishell *minishell, t_cmd *cmd,
+		t_command_type type, t_pipeline *pipeline)
+{
+	execute_builtin(minishell, cmd, type, pipeline->n);
+	free_pipeline(pipeline);
+	ft_lstclear(&(minishell->env_lst), free_env_wrapper);
+	free(minishell->cwd);
+	exit(minishell->last_status);
+}
+
+static void	setup_child_io(t_minishell *minishell, t_pipeline *pipeline,
+		int pos, t_cmd *cmd)
+{
+	t_status	status;
+	t_redir_err	err;
+
+	status = pipe_duplicate(pipeline, pos);
+	close_pipes(pipeline->pipes, pipeline->n - 1);
+	if (status != SUCCESS)
+		exit_error(minishell, pipeline, "dup", status);
+	err.status = SUCCESS;
+	err.redir_err = NULL;
+	redirect(minishell, cmd->redir_lst, &err);
+	if (err.status != SUCCESS)
+		handle_redir_error(minishell, pipeline, err);
+}
+
+void	run_in_child(t_minishell *minishell, t_pipeline *pipeline, int pos)
+{
+	t_status		status;
+	t_cmd			*cmd;
+	t_command_type	type;
+
+	cmd = get_cmd_from_lst(pipeline->cmd_lst, pos);
+	setup_child_io(minishell, pipeline, pos, cmd);
+	type = scan_command_type(cmd);
+	status = set_underscore_for_invocation(minishell, cmd, type);
+	if (status != SUCCESS)
+		exit_error(minishell, pipeline, "malloc", ERR_MALLOC);
+	if (type != EXTERNAL && type != NO_CMD)
+		execute_builtin_in_child(minishell, cmd, type, pipeline);
+	else if (type == NO_CMD)
+		exit_success(minishell, pipeline);
+	else
+		execute_external_command(minishell, pipeline, cmd);
 }
