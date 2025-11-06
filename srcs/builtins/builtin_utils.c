30d53698c24a88be401697681cb821d44d464e78
Dminin2 <apnaeom22@gmail.com>
2025-11-06 14:48:46 +0900

    if文を削除してreturn(2)に修正

--- srcs/builtins/builtin_utils.c
+++ srcs/builtins/builtin_utils.c
@@ -6,7 +6,7 @@
 /*   By: aomatsud <aomatsud@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/08 18:41:27 by hmaruyam          #+#    #+#             */
-/*   Updated: 2025/11/06 13:34:09 by aomatsud         ###   ########.fr       */
+/*   Updated: 2025/11/06 14:48:01 by aomatsud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 int	return_error(t_minishell *minishell, char *msg, t_status status)
 {
 	print_error_msg(msg, status);
-	if (status == ERR_MALLOC)
-		minishell->should_exit = 1;
-	return (1);
+	minishell->should_exit = 1;
+	return (2);
 }
